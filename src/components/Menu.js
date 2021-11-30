af548a5667f5edc91b95125a8c77a169a909e107
DL <neoxrock@gmail.com>
2021-11-30 16:12:41 +1100

    init components

---
+++ src/components/Menu.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+const Menu = () => {
+  return (
+    <ul>
+      <li>
+        <link to="/red">Red</link>
+      </li>
+      <li>
+        <link to="/blue">Blue</link>
+      </li>
+    </ul>
+  );
+};
+
+export default Menu;
